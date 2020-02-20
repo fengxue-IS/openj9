a6e3fac188906576f9f7b55aadc22e19be2bc81a
Jack Lu <Jack.S.Lu@ibm.com>
2020-02-20 15:33:08 -0500

    Update Copyright dates
    Signed-off-by: Jack Lu <Jack.S.Lu@ibm.com>

--- jcl/src/java.base/share/classes/java/lang/invoke/MethodTypeHelper.java
+++ jcl/src/java.base/share/classes/java/lang/invoke/MethodTypeHelper.java
@@ -1,6 +1,6 @@
 /*[INCLUDE-IF OPENJDK_METHODHANDLES]*/
 /*******************************************************************************
- * Copyright (c) 2019, 2019 IBM Corp. and others
+ * Copyright (c) 2020, 2020 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
