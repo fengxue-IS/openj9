56755473d7269d4a5a3bf8a3ffea1e771e212f59
Jason Feng <fengj@ca.ibm.com>
2021-01-13 13:56:22 -0500

    Update NPE message for OpenJ9 with OpenJDK MethodHandles
    Updated NPE message for MethodHandles related tests with stacktrace or
    similar:
    	at java.base/java.lang.invoke.LambdaForm$DMH/0x0000000000000000.invokeVirtual(LambdaForm$DMH)
    	at java.base/java.lang.invoke.LambdaForm$MH/0x0000000000000000.invoke(LambdaForm$MH)
    	at java.base/java.lang.invoke.LambdaForm$MH/0x0000000000000000.invoke_MT(LambdaForm$MH)
    The message need to be adjusted if the stacktrace in question is
    changed.
    
    Signed-off-by: Jason Feng <fengj@ca.ibm.com>

--- test/functional/Java14andUp/src/org/openj9/test/jep358/NPEMessageTests.java
+++ test/functional/Java14andUp/src/org/openj9/test/jep358/NPEMessageTests.java
@@ -346,14 +346,14 @@ public class NPEMessageTests {
 		nullField.nullField = new NPEMessageTests();
 	}
 
-	@Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Cannot invoke \"java.lang.invoke.MethodHandle.invokeExact\\(java.lang.String\\)\"")
+	@Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Cannot invoke \"java.lang.invoke.MethodHandle.linkToVirtual\\(java.lang.Object, java.lang.invoke.MemberName\\)\" because \"<parameter1>\" is null")
 	public void test_invokehandle() throws WrongMethodTypeException, Throwable {
 		String msg = null;
 		MethodHandle mh = MethodHandles.lookup().findVirtual(String.class, "length", MethodType.methodType(int.class));
 		int len = (int) mh.invokeExact(msg);
 	}
 
-	@Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Cannot invoke \"java.lang.invoke.MethodHandle.invoke\\(java.lang.String\\)\"")
+	@Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Cannot invoke \"java.lang.invoke.MethodHandle.linkToVirtual\\(java.lang.Object, java.lang.invoke.MemberName\\)\" because \"<parameter1>\" is null")
 	public void test_invokehandlegeneric() throws WrongMethodTypeException, ClassCastException, Throwable {
 		String msg = null;
 		MethodHandle mh = MethodHandles.lookup().findVirtual(String.class, "length", MethodType.methodType(int.class));
@@ -1520,14 +1520,14 @@ public class NPEMessageTests {
 		}
 	}
 
-	@Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Cannot invoke \"java.lang.invoke.MethodHandle.invoke\\(java.lang.String\\)\"")
+	@Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Cannot invoke \"java.lang.invoke.MethodHandle.linkToVirtual\\(java.lang.Object, java.lang.invoke.MemberName\\)\" because \"<parameter1>\" is null")
 	public void test_MHinvoke() throws Throwable {
 		String msg = null;
 		MethodHandle mh = MethodHandles.lookup().findVirtual(String.class, "length", MethodType.methodType(int.class));
 		mh.invoke(msg);
 	}
 
-	@Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Cannot invoke \"java.lang.invoke.MethodHandle.invokeExact\\(java.lang.String\\)\"")
+	@Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Cannot invoke \"java.lang.invoke.MethodHandle.linkToVirtual\\(java.lang.Object, java.lang.invoke.MemberName\\)\" because \"<parameter1>\" is null")
 	public void test_MHinvokeExact() throws Throwable {
 		String msg = null;
 		MethodHandle mh = MethodHandles.lookup().findVirtual(String.class, "length", MethodType.methodType(int.class));
